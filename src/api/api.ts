4ae737e48b35b79a46596995dc51ae02d9650f66
Fares Aouani Cherif <fares.aouanicherif@mazars.de>
2024-08-21 20:18:43 +0200

    cleaner code

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,1 +1,36 @@
-export const generatePrompt = async () => {};
+import axios from 'axios';
+
+export const generatePrompt = async prompt => {
+  try {
+    const apiKey = import.meta.env.VITE_OPENAI_API_KEY;
+
+    const res = await axios.post(
+      'https://api.openai.com/v1/chat/completions',
+      {
+        model: 'gpt-3.5-turbo',
+        messages: [
+          {
+            role: 'system',
+            content:
+              'I am going to give you my needs and you Write me a good solid prompt for chatgpt so it helps me figure out a path, be creative and useful. You are a great prompt engineer'
+          },
+          {
+            role: 'user',
+            content: prompt
+          }
+        ],
+        max_tokens: 550,
+        temperature: 0.7
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+    return res.data.choices[0].message.content;
+  } catch (error) {
+    console.error('Error sending request to OpenAI:', error);
+  }
+};
--- src/pages/form-page.tsx
+++ src/pages/form-page.tsx
@@ -6,9 +6,9 @@ import Button from '@mui/material/Button';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
-import axios from 'axios';
 import GeneratedPrompt from '../components/generated-prompt';
 import WaitingForAResponse from '../components/waiting-for-a-reponse';
+import { generatePrompt } from '../api/api';
 
 const FormPage = () => {
   const [descriptionInput, setDescriptionInput] = useState<string>('');
@@ -24,36 +24,8 @@ const FormPage = () => {
     )}`;
 
     try {
-      const apiKey = import.meta.env.VITE_OPENAI_API_KEY;
-
-      const res = await axios.post(
-        'https://api.openai.com/v1/chat/completions',
-        {
-          model: 'gpt-3.5-turbo',
-          messages: [
-            {
-              role: 'system',
-              content:
-                'I am going to give you my needs and you Write me a good solid prompt for chatgpt so it helps me figure out a path, be creative and useful. You are a great prompt engineer'
-            },
-            {
-              role: 'user',
-              content: prompt
-            }
-          ],
-          max_tokens: 550,
-          temperature: 0.7
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${apiKey}`,
-            'Content-Type': 'application/json'
-          }
-        }
-      );
-
+      setResponse(await generatePrompt(prompt));
       setWaitingForAResponse(false);
-      setResponse(res.data.choices[0].message.content ?? '');
     } catch (error) {
       console.error('Error sending request to OpenAI:', error);
     }
